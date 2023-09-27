5c69a1fba009a44b887a1a2c229f339d62c13a9f
divazahra <dvazahr31@gmail.com>
2023-09-27 09:30:56 +0700

    jurnal 01

---
+++ Biodata.java
@@ -0,0 +1,7 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("Nama : Diva Zahra Fakhirah");
+        System.out.println("NIM : 607062330070");
+        System.out.println("Kelas : D3IF-47-04");
+    }
+}
